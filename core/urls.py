34974233fe715aef1629f016b5a8d24dfcb46649
unknown <https://github.com/qwertyio19/21-1b-hws_month_3_repeat.git>
2025-10-09 17:48:08 +0600

    push

---
+++ core/urls.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+from core.yasg import urlpatterns_yasg
+
+# Основные маршруты (не зависят от мультиязычности)
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", include(urlpatterns_yasg)),
+]
+
+
+# Маршруты, зависящие от языка (i18n)
+urlpatterns += i18n_patterns(
+
+)
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
